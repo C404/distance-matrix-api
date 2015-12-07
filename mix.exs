1c875989db2a056b2af259c412c6ec76c49c2603
Thibault Prat-Hagler <thibault.hagler@gmail.com>
2015-12-07 21:04:13 +0100

    Format mix and version for hex.pm

--- mix.exs
+++ mix.exs
@@ -1,16 +1,12 @@
 defmodule DistanceMatrixApi.Mixfile do
   use Mix.Project
 
-  @description """
-    Google Distance Matrix API Library for Elixir
-  """
-
   def project do
     [app: :distance_api_matrix,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0",
      name: "DistanceMatrixApi",
-     description: @description,
+     description: description,
      package: package,
      deps: deps,
      source_url: "https://github.com/C404/distance-api-matrix"]
@@ -20,6 +16,12 @@ defmodule DistanceMatrixApi.Mixfile do
     [applications: [:logger, :httpoison]]
   end
 
+  defp description do
+    """
+    Google Distance Matrix API Library for Elixir
+    """
+  end
+
   defp deps do
     [{:httpoison, "~> 0.8.0"},
     {:poison, "~> 1.5"},
@@ -27,8 +29,9 @@ defmodule DistanceMatrixApi.Mixfile do
   end
 
   defp package do
-    [ maintainers: ["C404"],
-      licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/C404/distance-api-matrix"} ]
+    [files: ~w(lib mix.exs README.md LICENSE VERSION),
+     maintainers: ["C404"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/C404/distance-api-matrix"}]
   end
 end
